a95549f95087df76217078a8ef04664e1b3d9fec
norah486 <32691116+norah486@users.noreply.github.com>
2024-12-09 23:01:03 -0300

    day 9: remove comments

--- day09/main.ts
+++ day09/main.ts
@@ -13,30 +13,13 @@ async function part1() {
 
     const converted: string[] = [];
     let id = 0;
-    // const ids = new Map<number, number>();
 
     for (let i = 0; i < full.length; i++) {
-        // if (i % 2 !== 0) converted += ".".repeat(Number(full[i]));
         if (i % 2 !== 0) {
             for (let j = 0; j < Number(full[i]); j++) {
                 converted.push(".");
             }
         } else {
-            // const id = ids.get(2) ? ids.get(2) : ids.set(2, 3);
-            // let id = ids.get(Number(full[i]));
-            // if (!id) {
-            //     ids.set(Number(full[i]), ids.size);
-            //     converted += (ids.size - 1).toString().repeat(Number(full[i]));
-            // } else {
-            //     converted += id.toString().repeat(Number(full[i]));
-            // }
-            // if (id >= 10) {
-            //     const id = ids.get(Number(full[i]));
-            //     converted += id!.toString().repeat(Number(full[i]));
-            // } else {
-            //     converted += id.toString().repeat(Number(full[i]));
-            //     ids.set(Number(full[i]), id);
-            // }
             for (let j = 0; j < Number(full[i]); j++) {
                 converted.push(id.toString());
             }
@@ -44,31 +27,18 @@ async function part1() {
         }
     }
 
-    // console.log(JSON.stringify(converted));
-
-    // console.log(ids);
-    // console.log(converted);
-
     const stored_positions: number[] = [];
     for (let i = converted.length - 1; i >= 0; i--) {
-        // console.log(`GOIN ${i} out of ${converted.length}`);
         if (stored_positions.includes(i - 1)) break;
         if (converted[i] !== ".") {
             const pos = converted.indexOf(".");
-            // converted = replaceAt(converted, converted[i], pos);
-            // converted = replaceAt(converted, ".", i);
             converted[pos] = converted[i];
             converted[i] = ".";
             stored_positions.push(pos);
         }
     }
 
-    // console.log(converted);
-
     const conv_num = converted.filter((v) => v !== ".");
-
-    // console.log(converted.toString().replaceAll(",", ""));
-    //converted = converted.replaceAll(".", "");
 
     let sum = 0;
     for (let i = 0; i < conv_num.length; i++) {
@@ -88,39 +58,19 @@ async function part2() {
 
     let converted: string[] = [];
     let id = 0;
-    // const positions = new Map<number, number[]>();
 
     for (let i = 0; i < full.length; i++) {
         if (i % 2 !== 0) {
-            // for (let j = 0; j < Number(full[i]); j++) {
-            // }
             converted.push(".".repeat(Number(full[i])));
-            //for (let j = 0; j < Number(full[i]); j++) {
-            //    converted.push(".");
-            //}
         } else {
-            // for (let j = 0; j < Number(full[i]); j++) {
-            //     // if (j === 0) positions.set(id, [converted.length]);
-            //     converted.push(id.toString());
-            //     // if (j === Number(full[i]) - 1) {
-            //     //     positions.get(id)!.push(converted.length - 1);
-            //     // }
-            // }
-            // positions.set(id, [])
             converted.push(`${id};`.repeat(Number(full[i])).slice(0, -1));
             id += 1;
         }
     }
 
-    //console.log(JSON.stringify(converted));
-    //console.log(positions);
-
-    // console.log(converted.filter((v) => v.includes("."))[0]);
-
     const stored_positions: number[] = [];
     const iterated_ids: number[] = [];
 
-    // console.log(JSON.stringify(converted));
     converted = [...converted.filter((v) => v.length > 0)];
 
     for (let i = 0; i < converted.length; i++) {
@@ -134,23 +84,10 @@ async function part2() {
 
     let new_one: string[] = [...converted];
 
-    // for (let i = positions.size - 1; i >= 0; i--) {
-    // console.log(positions.get(i));
-    //}
-
-    // console.log(JSON.stringify(converted));
-
-    //console.log();
     for (let i = converted.length - 1; i >= 0; i--) {
-        // if (stored_positions.includes(i - 1)) break;
         if (
             !converted[i].includes(".") &&
-            //!converted[i].split(";").includes(iterated_ids.toString()) &&
             !iterated_ids.includes(Number(converted[i].split(";")[1]))
-            //converted[i] !== "." // &&
-            // (i === converted.length - 1 ||
-            //     i === converted.length - 2 || i === converted.length - 3 ||
-            //     i === converted.length - 4)
         ) {
             const pos = new_one.indexOf(
                 new_one.filter((v) =>
@@ -159,21 +96,8 @@ async function part2() {
                 )[0],
             );
 
-            // console.log(
-            //     JSON.stringify(new_one),
-            //     pos,
-            //     pos === -1 ? "gone" : new_one[pos].length,
-            //     converted[i].length,
-            //     converted[i],
-            //     pos === -1
-            //         ? "discarded"
-            //         : converted[i].replaceAll(";", "").length <=
-            //             new_one[pos].length,
-            // );
-
             if (pos !== -1) {
                 const stored_value = converted[i];
-                // new_one = new_one.filter((v) => v !== new_one[i]);
                 const old_pos = new_one.findIndex((v) => v === stored_value);
                 if (pos < old_pos) {
                     new_one[old_pos] = ".".repeat(
@@ -187,49 +111,19 @@ async function part2() {
                     ) {
                         popIndex(new_one, pos + 1);
                     } else {
-                        // console.log(JSON.stringify(new_one));
-                        // console.log(new_one[pos + 1].length);
-                        // console.log(new_one[pos].replaceAll(";", "").length);
-
                         new_one[pos + 1] = new_one[pos + 1].slice(
                             new_one[pos].split(";").length,
                             new_one[pos + 1].length,
                         );
-
-                        // console.log(JSON.stringify(new_one));
                     }
                 }
                 stored_positions.push(pos);
                 iterated_ids.push(Number(stored_value.split(";")[0]));
             }
-
-            // if (converted[i].length <= new_one[pos].length) {
-            //     new_one = converted.toSpliced(pos, 0, converted[i]);
-            //     new_one[i + 1] = ".".repeat(converted[pos].length);
-            //     new_one[pos + 1] = converted[pos].slice(
-            //         0,
-            //         new_one[pos].length - 1,
-            //     );
-            //     stored_positions.push(pos);
-            // }
-            // if (converted.indexOf(".".repeat(converted[i].length)) !== -1) {
-            //     const pos = converted.indexOf(".".repeat(converted[i].length));
-            //     // converted = replaceAt(converted, converted[i], pos);
-            //     // converted = replaceAt(converted, ".", i);
-            //     converted[pos] = converted[i];
-            //     converted[i] = ".";
-            //     stored_positions.push(pos);
-            // }
         }
     }
-    /*
-console.log();
-console.log(JSON.stringify(new_one));
-// console.log(new_one.toString().replaceAll(",", ""));
-console.log(new_one.toString().replaceAll(",", "").replaceAll(";", ""));
-    */
+
     const by_id: string[] = [];
-    // const convertv2 = new_one.filter((v) => !v.includes("."));
 
     for (const value of new_one) {
         const split = value.split(";");
@@ -246,74 +140,14 @@ console.log(new_one.toString().replaceAll(",", "").replaceAll(";", ""));
         }
     }
 
-    // console.log(JSON.stringify(by_id));
-    // console.log(by_id.toString().replaceAll(",", "").replaceAll(";", ""));
-
     let sum = 0;
     for (let i = 0; i < by_id.length; i++) {
-        // console.log(conv_num);
         if (by_id[i] !== ".") {
-            // console.log(`${i} * ${by_id[i]}`);
             sum += i * Number(by_id[i]);
         }
     }
 
     return sum;
-
-    /*
-const stored_positions: number[] = [];
-for (let i = converted.length - 1; i >= 0; i--) {
-    console.log(`GOIN ${i} out of ${converted.length}`);
-    if (stored_positions.includes(i - 1)) break;
-    if (converted[i] !== ".") {
-        if (converted.indexOf(".".repeat(converted[i].length)) !== -1) {
-            const pos = converted.indexOf(".".repeat(converted[i].length));
-            // converted = replaceAt(converted, converted[i], pos);
-            // converted = replaceAt(converted, ".", i);
-            converted[pos] = converted[i];
-            converted[i] = ".";
-            stored_positions.push(pos);
-        }
-    }
-}
-    */
-
-    // const conv_num = new_one.filter((v) => !v.includes("."));
-    /*
-const conv_num = "00992111777.44.333....5555.6666.....8888..";
-console.log(conv_num);
-
-// console.log(converted.toString().replaceAll(",", ""));
-//converted = converted.replaceAll(".", "");
-
-let sum = 0;
-for (let i = 0; i < conv_num.length; i++) {
-    // console.log(conv_num);
-    if (conv_num[i] !== ".") sum += i * Number(conv_num[i]);
-}
-
-console.log(sum);
-    */
-
-    /*
-const test = ["2", "..", "....", "5"];
-
-console.log(test);
-for (let i = 0; i < test.length; i++) {
-    if (test[i].includes(".") && test[i - 1].includes(".")) {
-        test[i] = test[i].concat(test[i - 1]);
-        popIndex(test, i - 1);
-    }
-}
-
-console.log(test);
-    */
-
-    /*
-const test = ["2", "...", "33"];
-console.log(test[1] = test[1].slice(0, 1));
-console.log(test);
-    */
 }
 
 Deno.bench({
