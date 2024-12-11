ffff8b80865e859ace22c70bced37640d62a0dae
norah486 <32691116+norah486@users.noreply.github.com>
2024-12-11 20:06:10 -0300

    day11: better part1

--- day11/main.ts
+++ day11/main.ts
@@ -13,34 +13,17 @@ async function part1() {
         }
     }
 
-    const updated_stones: string[] = [...stones.map((v) => v.toString())];
+    let updated_stones: string[] = [...stones.map((v) => v.toString())];
     for (let i = 0; i < 25; i++) {
-        updated_stones.forEach((stone, j) => {
-            if (Number(stone) === 0) {
-                updated_stones[j] = "1";
-            } else if (stone.length % 2 === 0) {
-                const length = updated_stones[j].toString().length;
-                const l = Number(
-                    updated_stones[j].toString().slice(0, length / 2),
-                );
-                const r = Number(
-                    updated_stones[j].toString().slice(length / 2, length),
-                );
-                updated_stones[j] = `${l};${r}`;
-            } else {
-                updated_stones[j] = (Number(updated_stones[j]) * 2024)
-                    .toString();
-            }
+        updated_stones = updated_stones.flatMap((x) => {
+            if (x === "0") return x = "1";
+            else if (x.length % 2 === 0) {
+                return [
+                    `${Number(x.slice(0, x.length / 2))}`,
+                    `${Number(x.slice(x.length / 2, x.length))}`,
+                ];
+            } else return (Number(x) * 2024).toString();
         });
-
-        for (const x of updated_stones) {
-            if (x.includes(";")) {
-                const ind = updated_stones.findIndex((v) => v === x);
-                const d = x.split(";");
-                updated_stones[ind] = d[0];
-                updated_stones.splice(ind + 1, 0, d[1]);
-            }
-        }
     }
 
     return updated_stones.length;
